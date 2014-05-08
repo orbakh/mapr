c5eae9604de0f0b210e941040dbb6e093ca0128e
orbakh <matt_panik@me.com>
2014-05-08 11:48:33 -0400

    cleanup and aggregation addition
    moved what i could to functions and added an aggregate number calculation

--- check_volume_mapr.py
+++ check_volume_mapr.py
@@ -6,28 +6,34 @@ from subprocess import Popen, PIPE
 maprcli = "/opt/mapr/bin/maprcli"
 gmetric = "/usr/bin/gmetric"
 
-def main(argv):
-   grep = ''
+def options():
+   grep = "none"
    try:
-      opts, args = getopt.getopt(argv,"g:",["grep="])
-   except getopt.GetoptError:
-      print 'check_space_mapr.py -g <filesystem name> '
+      opts, args = getopt.getopt(sys.argv[1:], "g:h", ["help", "grep="])
+   except getopt.GetoptError as err:
+      print(err)
+      usage()
       sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print 'check_space_mapr.py -g <filesystem name>'
+   for opt, args in opts:
+      if opt in ("-h"):
+	 usage()
          sys.exit()
-      elif opt in ("-g", "--grep"):
-         grep = arg
-
-if __name__ == "__main__":
-   main(sys.argv[1:])
+      elif opt in ("-g"):
+         return args
 
 def excutablecheck(command): # checks if the file is excutable
 	if os.access(command, os.F_OK):
 		return
 	else: 
 		sys.exit
+
+def usage():
+	 print 'usage:'
+      	 print 'check_space_mapr.py -h '
+         print 'check_space_mapr.py -g <filesystem name>'
+	
+if __name__ == "__main__":
+	grep = options()
 
 
 ''' Check if the commands are excutable '''
@@ -39,13 +45,15 @@ maprcli+=" volume list -columns \"volumename,used\""
 
 ''' grab the maprcli output '''
 p = subprocess.Popen(maprcli, shell=True, stdout=PIPE, bufsize=1)
+aggregate = 0	
 for line in p.stdout.readlines():
-	searchObj = re.search('aps', line, flags=0)
+	searchObj = re.search(grep, line, flags=0)
 	if searchObj:
 		line.strip()	
 		status = 0
 		number,directory = line.split()
 		number = int(number)
+		aggregate += number
 		number =  number/1024 
 		if number > 1024:
 			status = 1
@@ -61,12 +69,34 @@ for line in p.stdout.readlines():
 			cmd += " --type=int16 --units=Gigabytes --group=volume_metrics"
 		else:
 			cmd += " --type=int16 --units=Terabytes --group=volume_metrics"
-			
+
 		o = subprocess.Popen(cmd, shell=True, stdout=PIPE, bufsize=1)
 		o.stdout.close()
 	  	o.wait()	
+aggregate = aggregate/1024
+if number > 1024:
+	aggregate = aggregate/1024
+	aggregate = str(aggregate)
+	cmd = gmetric
+	cmd += " --name db."
+	cmd += grep
+	cmd += " --value "
+	cmd += aggregate
+	cmd += " --type=int16 --units=Terabytes --group=volume_metrics"
+	o = subprocess.Popen(cmd, shell=True, stdout=PIPE, bufsize=1)
+	o.stdout.close()
+	o.wait()	
+else:
+	aggregate = str(aggregate)
+	cmd = gmetric
+	cmd += " --name db."
+	cmd += grep
+	cmd += " --value "
+	cmd += aggregate
+	cmd += " --type=int16 --units=Terabytes --group=volume_metrics"
+	o = subprocess.Popen(cmd, shell=True, stdout=PIPE, bufsize=1)
+	o.stdout.close()
+	o.wait()	
 
 p.stdout.close()
 p.wait()
-
-	
